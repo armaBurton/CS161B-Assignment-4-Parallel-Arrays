7477c7975588d634fe36c798c12383caa8c65dee
Arma Burton <arma@armaburton.com>
2023-07-21 11:36:44 -0700

    {readInput issues}

--- helper.cpp
+++ helper.cpp
@@ -5,16 +5,41 @@ void welcome(){
 }
 
 void readInput(char courseNums[][MAXCHAR], int students[], int &count){
-    char temp[MAXCHAR];
+    char temp[REALLY_BIG_NUMBER];
     size_t tempLength;
+    bool loopState;
 
     cout << "Enter course number and students enrolled when prompted.\n"
          << "Enter 'Quit' or 'quit' for course number when you are done.\n";
         
     do{
+        loopState = false;
+        // memset(temp, '\0', sizeof(temp));
+        cout << "Enter course number: ";
+        // cin.get(temp, '\n');
         cin >> temp;
-        cout << temp << endl;
-    }while(strcmp(temp, "quit") != 0 && strcmp(temp, "Quit") != 0);
+        cin.ignore('\n');
+        tempLength = strlen(temp);
+
+        if(tempLength > MAXCHAR){
+            cout << "Input exceeds bounds.\n";
+            loopState = true;
+        }
+
+        // if (tempLength > MAXCHAR){
+        //     cout << "Input exceeds bounds.\n";
+        // } else if (tempLength == 0){
+        //     cout << "Null input.\n";
+        //     cin.ignore();
+        // } else {
+        //     strcpy(courseNums[count], temp);
+        //     cout << courseNums[count] << endl;
+        //     readInt("Number of students enrolled: ", students[count]);
+        //     count++;
+        // }
+        // memset(temp, 0, sizeof(temp));
+        // cout << temp << endl;
+    }while(loopState);
 }
 
 void readInt(const char prompt[], int &num){
--- helper.h
+++ helper.h
@@ -8,9 +8,10 @@ using namespace std;
 const int MAXCHAR = 20;
 const int MAXCOURSE = 51;
 const int MAXCAPACITY = 5;
+const int REALLY_BIG_NUMBER = 999999;
 
 void welcome();
 void readInput(char courseNums[][MAXCHAR], int students[], int &count);
-// void readInt(const char prompt[], int &num);
+void readInt(const char prompt[], int &num);
 // void printList(char courseNums[][MAXCHAR], int students[], int count);
 // void cancelCourses(char courseNums[][MAXCHAR], int students[], int count);
