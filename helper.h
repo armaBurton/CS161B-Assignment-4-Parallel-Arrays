905c737cf0c61536b55cfe2c0082d11e99afeacc
Arma Burton <arma@armaburton.com>
2023-07-21 15:17:25 -0700

    {more comments}

--- helper.h
+++ helper.h
@@ -9,13 +9,13 @@
 
 using namespace std;
 
-const int MAXCHAR = 20;
-const int MAXCOURSE = 51;
-const int MAXCAPACITY = 5;
-const int REALLY_BIG_NUMBER = 999999;
+const int MAXCHAR = 20;                 //character limit for courseNums
+const int MAXCOURSE = 51;               //couse limit for courseNums
+const int MAXCAPACITY = 5;              //oversized number for students per class
+const int REALLY_BIG_NUMBER = 999999;   //A really big number to prevent crashing
 
-void welcome();
-void readInput(char courseNums[][MAXCHAR], int students[], int &count);
-void readInt(const char prompt[], int &num);
-void printList(char courseNums[][MAXCHAR], int students[], int count);
-void cancelCourses(char courseNums[][MAXCHAR], int students[], int &count);
+void welcome(); //Hi
+void readInput(char courseNums[][MAXCHAR], int students[], int &count); //read cstring input
+void readInt(const char prompt[], int &num);    //read int input
+void printList(char courseNums[][MAXCHAR], int students[], int count);  //prints lists in prallel
+void cancelCourses(char courseNums[][MAXCHAR], int students[], int &count); //edits lists in parallel
